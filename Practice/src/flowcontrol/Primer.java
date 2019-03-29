36acc43746fe9251c582fef249b5ed0e3314a4ca
Gh05ts <48982172+Gh05ts@users.noreply.github.com>
2019-03-30 00:21:33 +0530

    Update Primer.java

--- Practice/src/flowcontrol/Primer.java
+++ Practice/src/flowcontrol/Primer.java
@@ -5,31 +5,26 @@ public class Primer {
 	static boolean isPrime(int n) {
 		
 		if(n <= 1)
-			return false;
-		
+			return false;	
 		for(int i = 2;i < n;i++)
 			if(n % i == 0)
 				return false;
-		
 		return true;		
 	}
 	
 	static void printPrime(int n) {
 		
 		for(int i = 2; i <= n; i++)
-			
 			if(i == 2)
 				System.out.print(2);
 			else if(isPrime(i)) {
 				System.out.print(", "+i);
-
 			}
 	}
 	
 	static public void main(String args[]) {
 		
 		int n = 50;
-
 		printPrime(n);
 	}
 	
