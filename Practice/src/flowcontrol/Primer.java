121df156dbbeadf0b00aceae09511cb8bd1f6c74
Gh05ts <48982172+Gh05ts@users.noreply.github.com>
2019-03-30 00:23:17 +0530

    Update Primer.java

--- Practice/src/flowcontrol/Primer.java
+++ Practice/src/flowcontrol/Primer.java
@@ -6,7 +6,7 @@ public class Primer {
 		
 		if(n <= 1)
 			return false;	
-		for(int i = 2;i < n;i++)
+		for(int i = 3;i < n;i++)
 			if(n % i == 0)
 				return false;
 		return true;		
@@ -14,7 +14,7 @@ public class Primer {
 	
 	static void printPrime(int n) {
 		
-		for(int i = 2; i <= n; i++)
+		for(int i = 2; i < n; i++)
 			if(i == 2)
 				System.out.print(2);
 			else if(isPrime(i)) {
